bea21577edf70d4b560b3b98d2c037e98b58862d
Camille Roussel <camille@rousselfamily.com>
2015-02-13 20:43:23 -0800

    test

--- client/app/app.js
+++ client/app/app.js
@@ -6,8 +6,8 @@
   function initialize(elementId){
 
     var runner = Runner(elementId);
-    console.log(runner)
-    runner.update('Check out HackReactor');
+    console.log(runner);
+    runner.update('25 years of political money');
 
     ///////////////////////
 
