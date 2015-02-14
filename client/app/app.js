73b446424b94d60f363a750043db7276178d70e4
Camille Roussel <camille@rousselfamily.com>
2015-02-13 22:01:38 -0800

    entry

--- client/app/app.js
+++ client/app/app.js
@@ -22,11 +22,11 @@ setTimeout(function(){
     .attr("cy", function(d) { return d.y; });
   }
 
-  var year1990 = data[4].slice();
+  var year1990 = data.slice();
   var counter = 0;
 
   var interval = setInterval(function() {
-    console.log(year1990[counter]);
+    console.log(year1990[counter].name);
     var d = {
       x: w / 2 + 2 * Math.random() - 1,
       y: h / 2 + 2 * Math.random() - 1,
@@ -35,7 +35,7 @@ setTimeout(function(){
 
     svg.append("svg:circle")
       .data([d])
-      .attr("r", function(d){return d.amount/100000})
+      .attr("r", function(d){return d.amount/1000000})
       .transition()
       .ease(Math.sqrt);
       //.attr("r", 4.5);
@@ -45,5 +45,5 @@ setTimeout(function(){
     if (counter > 99) clearInterval(interval);
     force.start();
   }, 30);
-}, 7000);
+}, 10);
 
--- client/app/data.js
+++ client/app/data.js
@@ -1,21 +1,26 @@
-var years = [];
+// var years = [];
 var data  = [];
-for(var i = 1990; i<2015; i++){
-	years.push(i);
-}
+// for(var i = 1990; i<2015; i++){
+// 	years.push(i);
+// }
 
 
-for(var j = 0; j<years.length; j++){
-	var url = 'http://transparencydata.com/api/1.0/aggregates/pols/top_100.json?cycle='+years[j]+'&apikey=2de79c8522f94e33b12d0f1ff179ece0';
-	$.ajax({
-		crossDomain: true,
-		url:url,
-		type:'GET',
-		success:function(d){
-			data.push(d);
-		},
-		dataType: "jsonp"
-	})
+// for(var j = 0; j<years.length; j++){
+// 	var url = 'http://transparencydata.com/api/1.0/aggregates/pols/top_100.json?cycle='+years[j]+'&apikey=2de79c8522f94e33b12d0f1ff179ece0';
+// 	$.ajax({
+// 		crossDomain: true,
+// 		url:url,
+// 		type:'GET',
+// 		success:function(d){
+// 			data.push(d);
+// 		},
+// 		dataType: "jsonp"
+// 	})
+// }
+
+for (var i = 0; i < 100; i += 1) {
+	data.push({amount: Math.floor(Math.random() * 9000000),
+		name: chance.name()});
 }
 
 console.log(data);
